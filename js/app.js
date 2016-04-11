ee4fa0dad9bbf93d0c15964cca3858d2bb0677e9
Christian Di Guardia <cguardia@gmail.com>
2016-04-11 12:22:49 -0300

    Mostrar error en sincronizar datos

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ var app =
         //ENTITY_TALLY_ID: 64,
 
         formStart: null, // Hora que empieza la carga del formulario
-        
-        init: function () { 
-            if (app.checkToken(function () { app.showMain() }, function () { app.showLogin() }));
+
+        init: function() {
+            if (app.checkToken(function() { app.showMain() }, function() { app.showLogin() }));
         },
 
-        showLogin: function () {
+        showLogin: function() {
             $("#divLogin").show();
             $("#divLinksHeaders").hide();
             $("#divMain").hide();
@@ -31,9 +31,9 @@ var app =
             $("#divTally").hide();
         },
 
-        showMain: function (msg) {
+        showMain: function(msg) {
             if (!app.isStaticDataLoaded) {
-                app.loadStaticData(function() {$("#divMain").show();});
+                app.loadStaticData(function() { $("#divMain").show(); });
             } else {
                 $("#divMain").show();
             }
@@ -52,52 +52,58 @@ var app =
             app.images = {};
         },
 
-        showTally: function () {
+        showTally: function() {
             $("#divMain").hide();
             $("#divTally").show();
             $("#tallyError").hide();
-            app.formStart = new Date(); 
-            
+            app.formStart = new Date();
+
         },
 
-        showHideNroContenedor: function () {
-            if ($("#tTipoDeVehiculo").val().indexOf("Contenedor")==0) {
-                $("#tNroContenedor").show();    
+        showHideNroContenedor: function() {
+            if ($("#tTipoDeVehiculo").val().indexOf("Contenedor") == 0) {
+                $("#tNroContenedor").show();
             } else {
                 $("#tNroContenedor").hide();
             }
         },
-        
-        checkToken: function (ok, noOk) {
+
+        checkToken: function(ok, noOk) {
             var token = libs.getToken();
             if (!token) { noOk(); }// No tenía token
             else { libs.callRPC({ url: "/api/accountapi/nop", callback: ok, errorCallback: noOk }); }
         },
 
-        loadStaticData: function (callback) {
+        loadStaticData: function(callback) {
             $("#divSincronizing").show();
             libs.callRPC(
                 {
                     url: "/api/entitiesApi/GetRootDropdowns",
                     data: { id: app.ENTITY_TALLY_ID, schemaId: app.SCHEMA_ID },
-                    callback: function (data) {
+                    callback: function(data) {
+                        try {
+                            $.each(data[app.ID_CLIENTE], function() {
+                                $("#tCliente").append("<option value='" + this.value + "'>" + this.text + "</option>");
+                            });
 
-                        $.each(data[app.ID_CLIENTE], function () {
-                                        $("#tCliente").append("<option value='" + this.value + "'>" + this.text + "</option>");
-                                    });
-
-                        $.each(data[app.ID_TIPO_UNIDAD], function () {
-                            $("#tTipoDeUnidad").append("<option value='" + this.value + "'>" + this.text + "</option>");
-                        });
-                        $("#divSincronizing").hide();
-                        callback();
-
+                            $.each(data[app.ID_TIPO_UNIDAD], function() {
+                                $("#tTipoDeUnidad").append("<option value='" + this.value + "'>" + this.text + "</option>");
+                            });
+                            $("#divSincronizing").hide();
+                            callback();
+                        }
+                        catch (ex) {
+                            alert("Error al cargar los datos sincronizados " + ex);
+                        }
+                    },
+                    errorCallback: function(error) {
+                        alert("Error al buscar datos sincronizados " + error);
                     }
                 });
 
         },
 
-        login: function () {
+        login: function() {
             // app.showMain();
             $("#butLogin").hide();
             $("#divErrorLogin").hide();
@@ -111,7 +117,7 @@ var app =
                     grant_type: "password"
                 },
                 type: "POST",
-            }).done(function (data) {
+            }).done(function(data) {
                 libs.setToken(data.access_token);
                 $("#butLogin").show();
                 $("#loadingLogin").hide();
@@ -122,7 +128,7 @@ var app =
                     $("#divErrorLogin").text("Usuario o contraseña inválidos");
                 }
             })
-                .fail(function (jqXHR, textStatus) {
+                .fail(function(jqXHR, textStatus) {
                     $("#butLogin").show();
                     $("#loadingLogin").hide();
                     $("#divErrorLogin").show();
@@ -130,18 +136,18 @@ var app =
                 });
         },
 
-        logout: function () {
+        logout: function() {
             app.username = "";
             app.password = "";
             app.showLogin();
         },
 
-        submitTally: function () {
+        submitTally: function() {
             $("#tallyError").hide();
             $("#uploading").hide();
             $("#loadinguUpload").show();
             if ($("#TALL_FILE_1").hasClass("btn-success") && $("#TALL_FILE_2").hasClass("btn-success") && $("#TALL_FILE_3").hasClass("btn-success") && $("#TALL_FILE_4").hasClass("btn-success") && $("#TALL_FILE_5").hasClass("btn-success") && $("#TALL_FILE_6").hasClass("btn-success") && $("#TALL_FILE_7").hasClass("btn-success")) {
-               
+
                 libs.callRPC(
                     {
                         url: "api/instancesapi/SaveInstanceFull"
@@ -164,14 +170,14 @@ var app =
                                 TALL_OBSERVACION_3: $("#TALL_FILE_10Obs").val(),
                                 TALL_OBSERVACION_4: $("#TALL_FILE_11Obs").val(),
                                 TALL_OBSERVACION_5: $("#TALL_FILE_12Obs").val(),
-                                TALL_INICIO_DE_DESCARGA: app.formStart,
-                                TALL_FINALIZACION_DE_DESC: new Date()
+                                TALL_INICIO_DE_DESCARGA: app.formatTime(app.formStart),
+                                TALL_FINALIZACION_DE_DESC: app.formatTime(new Date())
                                 //TALL_PALLETS: $("#tPallets").val()//esto tambien es una formula
                             })
                             , jsonNewFiles: JSON.stringify(app.images)
                             , jsonRemovedFiles: JSON.stringify([])
                         }
-                        , callback: function (c) {
+                        , callback: function(c) {
                             if (c.ok) {
                                 app.showMain("Tally subido con éxito");
 
@@ -218,7 +224,7 @@ var app =
                                 $("#loadinguUpload").hide();
                             }
                         }
-                        , errorCallback: function (e) {
+                        , errorCallback: function(e) {
 
                             $("#tallyError").text("Error al subir el tally " + e);
                             $("#tallyError").show();
@@ -235,12 +241,12 @@ var app =
             }
         },
 
-        capturePhoto: function (imgBut) {
+        capturePhoto: function(imgBut) {
             $(imgBut).addClass("btn-warning");
 
             try {
                 navigator.camera.getPicture(
-                    function (fileURI) {
+                    function(fileURI) {
                         app.uploadFilePG(fileURI, imgBut);
                         // window.resolveLocalFileSystemURI(fileURI,
                         //function (fileEntry) {
@@ -248,15 +254,19 @@ var app =
                         //},
                         //function (e) { alert("Error subiendo la foto " + e); });
                     }
-                    , function () { alert("error"); }
+                    , function() { alert("error"); }
                     , { destinationType: window.Camera.DestinationType.FILE_URI }
-                    );
+                );
             }
             catch (ex) { alert(ex); }
         },
 
+        formatTime: function(d) {
+            return d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+        },
+
         // Método que puede servir para probarlo en el escritorio
-        uploadFile: function (file, imgBut) {
+        uploadFile: function(file, imgBut) {
             var data = new FormData();
             data.append('file-0', file);
             data.append("schemaId", app.SCHEMA_ID);
@@ -268,20 +278,20 @@ var app =
                 processData: false,
                 data: data,
                 post: true,
-                callback: function (data) {
+                callback: function(data) {
                     app.images[imgBut.id] = file.name;
                     $(imgBut).removeClass("btn-warning");
 
                     $(imgBut).addClass("btn-success");
                 },
-                errorCallback: function (error) {
+                errorCallback: function(error) {
                     $(imgBut).removeClass("btn-warning");
                     $(imgBut).addClass("btn-alert");
                 }
             });
         },
 
-        uploadFilePG: function (imageURI, imgBut) {
+        uploadFilePG: function(imageURI, imgBut) {
             var fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
             var options = new FileUploadOptions();
             options.fileKey = "file";
@@ -303,7 +313,7 @@ var app =
 
             var ft = new FileTransfer();
             ft.upload(imageURI, encodeURI(app.SERVER_URL + "api/instancesApi/SaveFile"),
-                function (result) {
+                function(result) {
                     app.images[imgBut.id] = fileName;
                     if ($(imgBut).hasClass("btn-default")) {
                         $(imgBut).removeClass("btn-default");
@@ -312,17 +322,17 @@ var app =
                     $(imgBut).removeClass("btn-warning");
 
                 },
-                function (error) {
+                function(error) {
                     alert("Error al subir el archivo");
                     $(imgBut).addClass("btn-alert");
                     $(imgBut).removeClass("btn-warning");
                 },
                 options
-                );
+            );
         }
     }
 
-$(function () { app.init(); });
+$(function() { app.init(); });
 
 function handleFiles(files) {
     var file = files[0];
